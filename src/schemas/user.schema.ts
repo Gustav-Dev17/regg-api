f0a59f146308d950c68cee7e7e8935e5a26068ab
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-09-18 11:43:40 -0400

    Adding new field to body types, schema and services

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,6 +2,8 @@ import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
   name: yup.string().min(5, "Username must be longer than 5 characters!").required("Username is required!"),
+  cpf: yup.string().min(11, "CPF must be 11 characters long!!").required("CPF is required!"),
+  phone: yup.string().min(11, "Phone number must include DDD!").required("Phone number is required!"),
   email: yup.string().email().required("Email address is required!"),
-  password: yup.string().min(8, "Password must at least 8 characters long!").required("Password is required!"),
+  password: yup.string().min(8, "Password must be at least 8 characters long!").required("Password is required!"),
 });
