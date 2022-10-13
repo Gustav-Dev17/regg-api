d5478ceeeb176c82d40b2e61c448c6ea69f1eb43
andrebatista2 <andrelucas.batista@outlook.com>
2022-10-13 19:13:54 -0400

    ✨ Adicionado arquivo de imagens de usuário

---
+++ src/repositories/avatarImages.repository.ts
@@ -0,0 +1,28 @@
+import prisma from "services/prisma.services";
+import { IAvatarImages, IRequestAvatarImagesBody } from "types/avatarImages.body.types";
+
+export const CreateAvatarImagesRepo = (body: IAvatarImages) => {
+    return prisma.avatarImages.create({
+        data: body,
+    });
+}
+
+export const ReadAvatarImagesRepo = () => {
+    return prisma.avatarImages.findMany();
+}
+
+export const ReadAvatarImagesByIdRepo = (id: string) => {
+    try {
+        return prisma.avatarImages.findUnique({ where: { id } });
+    } catch (e) {
+        throw new Error((e as Error).message);
+    }
+}
+
+export const ReadAvatarImagesByUserId = (userId: string) => {
+    try {
+        return prisma.avatarImages.findFirst({ where: { userId } });
+    } catch (e) {
+        throw new Error((e as Error).message);
+    }
+}
