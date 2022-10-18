5f387f181b76c5b221ef9b30f35efc1fe54fc413
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-10-18 07:46:30 -0400

    🐛 Solve conflict

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import itemRoutes from "routes/item.routes";
 import userRoutes from "routes/user.routes";
 import transporterRoutes from "routes/transporter.routes";
 import furnitureImagesRoutes from "routes/furnitureImages.routes";
+import avatarImagesRoutes from "routes/avatarImages.routes";
 import deliveryRoutes from "routes/delivery.routes";
 import googleAuth from "routes/auth.google.routes";
 
@@ -18,10 +19,12 @@ app.use(
 );
 app.use(express.json());
 app.use("/furniture-images", express.static("furniture-images"));
+app.use("/avatar-images", express.static("avatar-images"));
 app.use(itemRoutes);
 app.use(userRoutes);
 app.use(transporterRoutes);
 app.use(furnitureImagesRoutes);
+app.use(avatarImagesRoutes);
 app.use(deliveryRoutes);
 app.use(googleAuth);
 
