1cda7c8c70be3bd1bec27bc413696b8027f08688
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-11-12 21:43:17 -0400

    🐛 Fix schema bug

--- src/schemas/delivery.schema.ts
+++ src/schemas/delivery.schema.ts
@@ -6,6 +6,7 @@ export const deliverySchema = yup.object().shape({
   origin_desc: yup.string().required("Origin description is required!"),
   destine: yup.string().required("Destine coordinates are required!"),
   destine_desc: yup.string().required("Destine description is required!"),
+  distance: yup.string().required("Distance is required!"),
   userId: yup.string().notRequired(),
   selectedItemsId: yup.string().required("selected items are required to create a delivery!"),
   transporterId: yup.string().required("A transporter ID is required to create a delivery!"),
