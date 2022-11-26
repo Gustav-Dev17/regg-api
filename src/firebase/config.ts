ef21f50c9f35ffe99962f9253602ba2ca0ae0e93
Douglas Filipe <douglas.colares@eunerd.com.br>
2022-11-26 12:45:32 -0400

    fix: change config firebase to import

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
-const serviceAccount = require("./regg-86e2b-firebase-adminsdk-zauge-6c5a6b6600.json");
 import firebaseAdmin from "firebase-admin";
+import serviceAccount from "./regg-86e2b-firebase-adminsdk-zauge-6c5a6b6600.json"
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
+  credential: firebaseAdmin.credential.cert(serviceAccount as any),
   storageBucket: "gs://regg-86e2b.appspot.com",
 });
 const bucket = firebaseAdmin.storage().bucket();
