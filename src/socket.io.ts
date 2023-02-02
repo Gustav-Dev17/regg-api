84ed82746e2efd18939a42cddea31bd2aedbe5cf
Douglas Filipe <douglas.colares@eunerd.com.br>
2023-02-02 10:13:51 -0400

    fix: refactr socket

--- src/socket.io.ts
+++ src/socket.io.ts
@@ -4,8 +4,11 @@ import { io } from "./app";
 export class SocketIO {
   static webSocket(app: Express) {
     io.on("connection", (socket) => {
-      socket.on("join", ({ deliveryId }) => {
+      socket.on("join", ({ deliveryId, userId }) => {
         socket.join(deliveryId);
+        if (userId) {
+          socket.join(userId);
+        }
       });
 
       socket.on("changeStatusForClient", ({ deliveryId }) => {
@@ -14,6 +17,10 @@ export class SocketIO {
 
       socket.on("changeStatusForTransporter", ({ deliveryId, cancel }) => {
         io.to(deliveryId).emit("sendToTransporter", { cancel });
+      });
+
+      socket.on("updateSolicitations", ({ userId }) => {
+        io.to(userId).emit("sendUpdate");
       });
     });
   }
