8a3c4b1ba7d3532fbf6afee47280e08b61c6fe2d
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2023-02-26 18:25:07 -0400

    Update payment.routes.ts

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -8,7 +8,10 @@ const route = Router();
 route.post("/payment", AuthMiddleware, WantPayment);
 route.patch("/payment/simulate", AuthMiddleware, SimulatePaymentDelivery);
 
-route.post("/payment/webhook", (req, res) => {
+route.post("/payment/webhook", AuthMiddleware, (req, res) => {
+  const { id } = req;
+  if (id !== null && id !== "63fbd8fa3c2607578c917d78") throw "Invalid user";
+
   return res.status(200).json("Hello, world!");
 });
 
